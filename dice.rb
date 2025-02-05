49e0a17ecb3988dbaee49eeebe2680f95ec033db
Shuhaku <Shuhaku7@icloud.com>
2025-02-05 22:27:01 +0900

    Change tarz sha256 hash

--- dice.rb
+++ dice.rb
@@ -2,7 +2,7 @@ class Dice < Formula
   desc "A fun CLI tool that makes random decisions Pachinko-style."
   homepage "https://github.com/shuhaku/dice-cli"
   url "https://github.com/shuhaku/dice-cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "b521a56b73705040de4c3a6623e15ce9e3698b54859866137a16ee83b3bc8ae2"
+  sha256 "014b945bb690f23f0eb7b70c37a4ed176a3eab00cb324a5e7ca8695792402756"
   license "MIT"
 
   depends_on "node"
